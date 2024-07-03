9894143175a7a2e3216b55c7786808fe84f20d5d
ollydeee <orenugaolamide@gmail.com>
2024-07-03 16:30:37 +0100

    deleted all your rubish

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App({ name }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="">
+      <header>
+        <h1>Welcome to eTech</h1>
       </header>
+
+      <section>
+        <p className="">Hello, world!</p>
+        <div className="flex">
+          <div className="">
+            <button>hello</button>
+          </div>
+          <div className="">
+            <button>hello</button>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
